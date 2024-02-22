bb96673a5d1ab9a3a33814606761851818f97e5d
Bipul Lamsal <bipullamsal@gmail.com>
2024-02-22 23:35:03 +0545

    [update] firestore added

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAAgBnuXOBxKveZvn8Ftr-0hHA65JLhvCA",
+
+  authDomain: "spotify-dealer.firebaseapp.com",
+
+  projectId: "spotify-dealer",
+
+  storageBucket: "spotify-dealer.appspot.com",
+
+  messagingSenderId: "21449624735",
+
+  appId: "1:21449624735:web:f6732622d026cf9d3026aa",
+
+  measurementId: "G-MRD9CH937B",
+};
+export default firebaseConfig;
--- src/pages/api/callback.ts
+++ src/pages/api/callback.ts
@@ -2,6 +2,9 @@ import parseEnv from "@/utility/EnvParser";
 import { NextApiRequest, NextApiResponse } from "next";
 import querystring from "querystring";
 import axios from "axios";
+import { db } from "../../../firebaseAuth";
+
+const SPOTIFY_COLLECTION_NAME = "spotify_tokens";
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,6 +13,7 @@ export default async function handler(
   try {
     const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_REDIRECT_URI } =
       await parseEnv();
+
     const code = req.query?.code ?? null;
     const response = await axios.post(
       "https://accounts.spotify.com/api/token",
@@ -26,11 +30,80 @@ export default async function handler(
         },
       }
     );
-    const { access_token } = response.data;
-    res.redirect(`/dashboard?access_token=${access_token}`);
-    // redirect(req, 302, `/dashboard?access_token=${access_token}`);
+    const { access_token, refresh_token } = response.data;
+    // initial fetch to user id to store to firestore
+    const userId = await fetchUserId(access_token);
+    if (!userId) {
+      return res.status(500).json({ error: "Failed to fetch Spotify user ID" });
+    }
+    const storedSuccessfully = await storeSpotifyToken(
+      userId,
+      access_token,
+      refresh_token
+    );
+
+    if (storedSuccessfully) {
+      res.redirect(`/dashboard?access_token=${access_token}`);
+    } else {
+      res.status(500).json({ error: "Failed to store Spotify tokens" });
+    }
   } catch (err) {
     console.error("Error in Spotify token request:", err);
     res.status(500).json({ error: "Internal Server Error" });
   }
 }
+
+const fetchUserId = async (access_token: string): Promise<string | null> => {
+  try {
+    const response = await fetch("https://api.spotify.com/v1/me", {
+      method: "GET",
+      headers: { Authorization: `Bearer ${access_token}` },
+    });
+
+    if (response.ok) {
+      const userData = await response.json();
+      return userData.id;
+    } else {
+      console.error("Error fetching Spotify user ID:", response.statusText);
+      return null;
+    }
+  } catch (err) {
+    console.error("Error fetching Spotify user ID:", err);
+    return null;
+  }
+};
+
+const storeSpotifyToken = async (
+  userId: string,
+  access_token: string,
+  refresh_token: string
+): Promise<boolean> => {
+  try {
+    if (!userId || !access_token || !refresh_token) {
+      return false;
+    }
+    const userRef = db.collection(SPOTIFY_COLLECTION_NAME).doc(userId);
+    const userSnapshot = await userRef.get();
+    if (userSnapshot.exists) {
+      // update document
+      await userRef.update({
+        access_token,
+        refresh_token,
+        timestamp: new Date(),
+      });
+    } else {
+      // new document
+      await db.collection(SPOTIFY_COLLECTION_NAME).doc(userId).set({
+        userId,
+        access_token,
+        refresh_token,
+        timestamp: new Date(),
+      });
+    }
+
+    return true;
+  } catch (err) {
+    console.error("Error storing Spotify tokens:", err);
+    return false;
+  }
+};
